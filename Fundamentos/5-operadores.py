c6cfc21960975888eae9562c81d8658c545801e0
Carlos Pereira <silvaotl@outlook.com>
2025-11-14 21:27:26 -0300

    Mais modificações no arquivo operadores.py

--- Fundamentos/5-operadores.py
+++ Fundamentos/5-operadores.py
@@ -1,1 +1,53 @@
 print("Sei que vou continuar.")
+num1 = int(input("Digite o primeiro número:\n"))
+num2 = int(input("Digite o segundo número:\n"))
+
+# Aritiméticos
+sum = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+
+mod = num1 % num2
+exp = num1 ** num2
+
+#print(sum)
+#print(sub)
+#print(div)
+#print(mult)
+#print(mod)
+#print(exp)
+
+print(f"A soma de {num1} mais {num2} é igual a: {sum}\n")
+print(f"A subtração entre {num1} e {num2} é: {sub}\n")
+print(f"A divisão entre {num1} e {num2} é: {div}\n")
+print(f"A Multiplicação entre {num1} e {num2} é: {mult}")
+print(f"O módulo entre os números {num1} e {num2} á: {mod}")
+print(f"A exponeciação entre {num1} e {num2} é: {exp}")
+
+# Comparação
+bigger = num1 > num2
+smaller = num1 < num2
+equal = num1 == num2
+differente = num1 != num2
+bigger_equal = num1 >= num2
+smaller_equal = num1 <= num2
+
+print(bigger)
+print(smaller)
+# Ou 
+print(f"Os números {num1} e {num2} são iguais? {equal}")
+print(f"O número {num1} é maior ou igual a {num2}? {bigger_equal}")
+print(f"O número {num1} é menor que {num2}? {smaller}")
+
+# Atribuição
+num1 += 1 # num1 = mun1 + 1
+num1 -= 1 # num1 = mun1 - 1
+num1 *= 1 # num1 = mun1 * 1
+num1 /= 1 # num1 = mun1 / 1
+
+
+
+
+
+
