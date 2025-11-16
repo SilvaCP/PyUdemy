b8cba5cd4dda7c36edd5a4d4eb9ded8088cf55d5
Carlos Pereira <silvaotl@outlook.com>
2025-11-16 20:21:46 -0300

    6-strings.py foi adicionado a Fundamentos

---
+++ Fundamentos/6-strings.py
@@ -0,0 +1,19 @@
+movieName = "Top Gun"
+movieName2 = "top gun"
+print(movieName == movieName2)# Case Sencitive.
+
+movieDescription = """
+    Top Gun Maverick é um filme de aviação e aventura
+muito consagrado na indústria
+"""
+# 1 - Multiplicação de Strings.
+
+line = "="
+print(line*56)
+print(movieName)
+print(movieDescription)
+
+# 2 - Procurar uma palavra dentro de um texto.
+
+print("Top" in movieName)
+print("ação" in movieName)
